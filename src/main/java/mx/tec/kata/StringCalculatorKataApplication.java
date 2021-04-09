c2c54c15e83b012e9eb8ca9e09695de195d93d9f
Karen Alicia Hernandez <karenaliciahernandez@hotmail.com>
2021-04-09 16:43:35 -0600

    first commit

---
+++ src/main/java/mx/tec/kata/StringCalculatorKataApplication.java
@@ -0,0 +1,13 @@
+package mx.tec.kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringCalculatorKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringCalculatorKataApplication.class, args);
+	}
+
+}
